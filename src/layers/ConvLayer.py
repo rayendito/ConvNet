25a69a70f24983025ca2d19e39d43d3031441b94
abiwardani <13519205@std.stei.itb.ac.id>
2022-10-07 18:50:16 +0700

    j-5

--- src/layers/ConvLayer.py
+++ src/layers/ConvLayer.py
@@ -90,7 +90,7 @@ class ConvLayer:
 
             self.weights += np.transpose(weight_updates)
 
-            self.biases += [np.sum(err_term_on_that_input[i]) for i in range(self.n_filters)]
+            self.biases += np.array([np.sum(err_term_on_that_input[i]) for i in range(self.n_filters)])
 
     # CONVOLUTION LAYER ERROR TERM
 
@@ -103,7 +103,7 @@ class ConvLayer:
         elif (preceding_layer_type == "Pooling"):
             self.error_term = -1*preceding_error_term*output_function_derivative
         elif (preceding_layer_type == "Convolution"):
-            intermediate_term = preceding_error_term*self._kernel_derivative(self.output[0])
+            intermediate_term = preceding_error_term*np.array([self._kernel_derivative(self.output[0]) for _ in self.output], dtype=object)
             self.error_term = -1*intermediate_term*output_function_derivative
         return self.error_term
     
