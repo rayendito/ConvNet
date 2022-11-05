fix backprop bugs

src/layers/ConvLayer.py
@@ -11,8 +11,8 @@ class ConvLayer:
         self.padding = padding
         self.input_shape = input_shape
         # self.output_shape = self.calculate_output_size()
-        self.bias = np.random.rand(self.n_filters)
-        self.bias = np.zeros(self.n_filters)
+        self.bias = np.random.rand(self.n_filters)/100
+        # self.bias = np.zeros(self.n_filters)
         self.input = []
         self.output = []
         self.layer_type = "Convolution"
@@ -106,11 +106,11 @@ class ConvLayer:
                     for k, nest_3 in enumerate(nest_2):
                         for l, nest_4 in enumerate(weight_updates[k]):
                             # deltas have duplicate error term at channel level
-                            weight_updates[k][l] = np.add(weight_updates[k][l], lr*nest_3*err_term_on_that_input[i][j][k], out=weight_updates[k], casting="unsafe")
+                            weight_updates[k][l] = np.add(weight_updates[k][l], lr*nest_3*err_term_on_that_input[i][j][k], out=weight_updates[k][l], casting="unsafe")
             # print(weight_updates)
             self.kernel += np.array(weight_updates, dtype=float)
 
-            self.bias += (np.array([np.sum(err_term_on_that_input[i]) for i in range(self.n_filters)]))*0
+            self.bias += (np.array([np.sum(err_term_on_that_input[i]) for i in range(self.n_filters)]))
 
     # CONVOLUTION LAYER ERROR TERM
 

src/layers/DenseLayer.py
@@ -73,8 +73,8 @@ class DenseLayer:
         return np.array([[0.1]*input_size]*output_size)
 
     def _initialize_biases(self, output_size):
-        return np.zeros(output_size)
-        return np.random.uniform(low=-1, high=1, size=output_size)
+        # return np.zeros(output_size)
+        return np.random.uniform(low=-0.001, high=0.001, size=output_size)
 
     def _test_initialize_biases(self, output_size):
         return np.array([0.2]*output_size)
@@ -113,12 +113,12 @@ class DenseLayer:
                 weight_update = lr*element*err_term_on_that_input
                 weight_updates.append(weight_update)
             w_update = np.transpose(weight_updates) + momentum*self.last_update_weights
-            self.weights += w_update
-            sum_update_weight += w_update
+            self.weights += np.sum(w_update, axis=0)
+            sum_update_weight += np.sum(w_update, axis=0)
 
-            b_update = (lr*err_term_on_that_input + momentum*self.last_update_biases)*0
-            self.biases += b_update
-            sum_update_bias += b_update
+            b_update = (lr*err_term_on_that_input + momentum*self.last_update_biases)
+            self.biases += np.sum(b_update, axis=0)
+            sum_update_bias += np.sum(b_update, axis=0)
         
         self.last_update_weights = sum_update_weight
         self.last_update_biases = sum_update_bias
@@ -148,8 +148,9 @@ class DenseLayer:
         sum_expressions = []
         preceding_weights = np.transpose(preceding_weights)
         for err in preceding_error_term:
-            sum_expressions.append([np.dot(err, weight) for weight in preceding_weights])
-        return np.array(sum_expressions)
+            # sum_expressions.append([np.dot(err, weight) for weight in preceding_weights])
+            sum_expressions.append([err*weight.T for weight in preceding_weights])
+        return np.sum(np.array(sum_expressions), axis=2)
     
     # ACTIVATION FUNCTION DERIVATIVE
 

src/layers/PoolingLayer.py
@@ -104,24 +104,9 @@ class PoolingLayer:
         elif (self.mode == PoolingLayer.AVG):
             self.derivatives = np.array([[[[1/(self.size*self.size) for _ in range(len(channels[0][0]))] for _ in range(0, len(channels[0]))] for _ in range(0, len(channels))] for channels in self.inputs])
         
-        self.error_term = [[] for _ in range(len(self.derivatives))]
-        for i, nest_1 in enumerate(self.derivatives):
-            temp_1 = [[] for _ in range(len(nest_1))]
-            for j, nest_2 in enumerate(nest_1):
-                temp_2 = [[] for _ in range(len(nest_2))]
-                for k, nest_3 in enumerate(nest_2):
-                    # print((preceding_weights*np.sum(preceding_error_term.T)).T.shape)
-                    sum_term = np.sum((preceding_weights*np.sum(preceding_error_term.T)).T, axis=0)
-                    # print(sum_term.shape)
-                    # print(nest_3.shape)
-                    temp_3 = nest_3*np.array([sum_term[k+k*l] for l in range(len(nest_3))])
-                    temp_2[k] = temp_3
-                
-                temp_1[j] = temp_2
-            
-            self.error_term[i] = temp_1
-
-        # self.error_term = np.array([[[nest_3*np.sum((preceding_weights*preceding_error_term.T).T, axis=0) for nest_3 in nest_2] for nest_2 in nest_1] for nest_1 in self.derivatives])
+        sum_term = np.sum((preceding_weights*np.sum(preceding_error_term.T)).T, axis=0)
+        self.error_term = np.array([[[nest_3*np.array([sum_term[k+k*l] for l in range(len(nest_3))]) for k, nest_3 in enumerate(nest_2)] for j, nest_2 in enumerate(nest_1)] for i, nest_1 in enumerate(self.derivatives)])
+        
         self.weights = None
     
     def get_all_weights(self):